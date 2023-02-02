6e7a0dc00d540cad6512bce95084db8fa73ece58
Michaela Procházková <prochazkovamichaela95@gmail.com>
2023-02-02 21:07:49 +0100

    mistake

--- main.js
+++ main.js
@@ -4,13 +4,14 @@ let jmeno = prompt ("Zadej své Jméno")
 let prijmeni = prompt ("Zadej své Přijmení")
 let rok = Number(prompt("Zadej rok svého narození"))
 let barva = prompt("Zadej oblíbenou barvu ANGLICKY")
-let letos = Number (2023)
-let vek = Number (letos - rok)
+let letos = 2023
+let vek = letos - rok
 
 vysledek =  jmeno +" "+ prijmeni +" "+ vek
 
-
 document.body.innerHTML = vysledek
+
+
 
 
 
