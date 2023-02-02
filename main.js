59683d437cd911d0c48c33a2c1a7b372a372abb5
Michaela Procházková <prochazkovamichaela95@gmail.com>
2023-02-02 21:03:09 +0100

    first_task

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 // tady je místo pro náš program
+let vysledek = document.querySelector(".vysledek")
+let jmeno = prompt ("Zadej své Jméno")
+let prijmeni = prompt ("Zadej své Přijmení")
+let rok = Number(prompt("Zadej rok svého narození"))
+let barva = prompt("Zadej oblíbenou barvu ANGLICKY")
+let letos = Number (2023)
+let vek = Number (letos - rok)
+
+vysledek =  jmeno +" "+ prijmeni +" "+ vek
+
+
+document.body.innerHTML = vysledek
+
+
+
+
+
+
 
 
 
